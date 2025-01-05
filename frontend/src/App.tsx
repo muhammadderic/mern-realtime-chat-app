2ff4d609fa57b8ce8bae8bac877ebc1be8359f02
muhammadderic <muhammad.donny.e@gmail.com>
2025-01-06 01:11:11 +0700

    setup routing, create initial signup and login pages and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,27 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
+  const authUser = false;
+
   return (
-    <div>
-      <h1>Hello Deric</h1>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <Routes>
+        <Route
+          path='/signup'
+          element={authUser ? <Navigate to='/' /> : <SignupPage />}
+        />
+        <Route
+          path='/login'
+          element={authUser ? <Navigate to='/' /> : <LoginPage />}
+        />
+        {/* catch all routes */}
+        <Route
+          path='*'
+          element={<Navigate to='/' replace />}
+        />
+      </Routes>
     </div>
   )
 }
