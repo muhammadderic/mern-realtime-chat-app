6b416b8fe2819511b0700e2a9faefacb963870a8
muhammadderic <muhammad.donny.e@gmail.com>
2025-01-07 01:11:11 +0700

    create emojis file and user conversation compt, and put it in user conversations compt

---
+++ frontend/src/components/sidebar/UserConversation.tsx
@@ -0,0 +1,42 @@
+import useConversationStore from "../../store/conversationStore";
+import type { User } from "../../types/types";
+
+type ConversationProps = {
+  user: User,
+  lastIdx: boolean,
+  emoji: string
+}
+
+const UserConversation = ({ user, lastIdx, emoji }: ConversationProps) => {
+  const { selectedUser, setSelectedUser } = useConversationStore();
+
+  const isSelected = selectedUser?._id === user._id;
+
+  return (
+    <>
+      <div
+        className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-sky-500" : ""}
+			`}
+        onClick={() => setSelectedUser(user)}
+      >
+        <div className={`avatar`}>
+          <div className='w-12 rounded-full'>
+            <img src={user.profilePic} alt='user avatar' />
+          </div>
+        </div>
+
+        <div className='flex flex-col flex-1'>
+          <div className='flex gap-3 justify-between'>
+            <p className='font-bold text-gray-200'>{user.fullName}</p>
+            <span className='text-xl'>{emoji}</span>
+          </div>
+        </div>
+      </div>
+
+      {!lastIdx && <div className='divider my-0 py-0 h-1' />}
+    </>
+  );
+};
+
+export default UserConversation;
--- frontend/src/components/sidebar/UserConversations.tsx
+++ frontend/src/components/sidebar/UserConversations.tsx
@@ -1,7 +1,22 @@
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../lib/utils/emojis";
+import UserConversation from "./UserConversation";
+
 const UserConversations = () => {
+  const { loading, users } = useGetConversations();
+
   return (
-    <div>
-      <h1>User Conversations</h1>
+    <div className='py-2 flex flex-col overflow-auto'>
+      {users.map((user, idx) => (
+        <UserConversation
+          key={user._id}
+          user={user}
+          emoji={getRandomEmoji() as string}
+          lastIdx={idx === users.length - 1}
+        />
+      ))}
+
+      {loading ? <span className='loading loading-spinner mx-auto'></span> : null}
     </div>
   )
 }
